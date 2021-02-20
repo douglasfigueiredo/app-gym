c1b57ef5c042d2e87d4125f705623c341df19e87
Douglas Figueiredo <douglasarts@gmail.com>
2021-02-19 22:05:04 -0300

    Adicionado logotipo, video institucional e ícones de objetivos

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   post '/cadastro', to: 'site#cadastro'
 
   root 'site#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
