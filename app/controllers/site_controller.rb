d3a80f9c81f23cb2547f4d7ad635213362d43207
Douglas Figueiredo <douglasarts@gmail.com>
2021-02-22 17:59:03 -0300

    Validando o formulário de cadastro

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,25 +1,36 @@
 class SiteController < ApplicationController
+  before_action :set_user
   layout 'site'
   
   def index;end
 
   def cadastro
+    
+    # if user_params
+    
+    # end
+      
+    # end
     # debugger
-    @cadastro_user = User.new
-    if verify_recaptcha
-      if params.blank? ==false
-        @cadastro_user = User.new(user_params)
-        @cadastro_user.save
-      else
-        redirect_to '/'
-      end
-
+    @user = User.new(user_params)
+    if verify_recaptcha(model: @user) && @user.save
+      flash[:success]= "Obrigado por se cadastrar na maisgym.com!"
+      redirect_to '/'  
     else
-      flash[:success] = "Erro ao verificar o captcha"
+      flash[:error] = @user.errors.full_messages[0].to_s
+      redirect_to '/#cadastro'
     end
+
+
   end
 
-  def user_params
-    params.permit(:nome, :email, :sexo)
-  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.all
+    end
+  
+    def user_params
+      params.permit(:nome, :email, :sexo)
+    end
 end
